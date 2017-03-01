a78a9502431ce279f221d32430010b1185553cc4
chenlu jie <chenlu jie>
2017-03-01 17:32:05 +0800

    v

--- video.php
+++ video.php
@@ -5,6 +5,6 @@
 	<title>视频播放</title>
 </head>
 <body>
-	<video src="video/1.mp4" controls=""></video>
+	<embed src='http://player.youku.com/player.php/sid/XMjUzNTUwOTIxNg==/v.swf' allowFullScreen='true' quality='high' width='480' height='400' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>
 </body>
 </html>
